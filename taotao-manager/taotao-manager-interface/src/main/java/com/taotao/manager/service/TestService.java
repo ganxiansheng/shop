7847bc16cd5f7f89233709e9b13086811bfa5e86
ganxiansheng <793017125@qq.com>
2019-03-13 22:41:28 +0800

    第一次提交

---
+++ taotao-manager/taotao-manager-interface/src/main/java/com/taotao/manager/service/TestService.java
@@ -0,0 +1,6 @@
+package com.taotao.manager.service;
+
+public interface TestService{
+    // 查询当前数据库时间
+    public String queryDate();
+}
---
+++ taotao-manager/taotao-manager-mapper/src/main/java/com/taotao/manager/mapper/TestMapper.java
@@ -0,0 +1,9 @@
+package com.taotao.manager.mapper;
+
+public interface TestMapper {
+
+    /**
+     * 查询数据库当前时间
+     */
+    public String queryDate();
+}
